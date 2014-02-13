cb2ea8453c42eac05616e015f8e60df869652f74
Maciej Pasternacki <maciej@3ofcoins.net>
2014-02-13 11:37:36 +0100

    Nicer diagnostics

--- idk-installer-run.sh
+++ idk-installer-run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 osarch=`uname -s | tr A-Z a-z`-`uname -m`
-export TMPDIR=`pwd`
 if [ -x ./idk-installer-$osarch ] ; then
     exec ./idk-installer-$osarch "${@}"
 else
-    echo -n "No installer found for $osarch. Available installers:" >&2
-    echo idk-installer-*-* | sed s/idk-installer-//g >&2
+    echo "No installer binary found for $osarch. Supported architectures: `echo idk-installer-*-* | sed s/idk-installer-//g`" >&2
     exit 1
 fi
